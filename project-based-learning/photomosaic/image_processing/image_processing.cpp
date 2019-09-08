7d7fc069dbd1a7e4b4fe0ab10d34250703b6e3f6
cescgina <cescgina@gmail.com>
2019-09-08 17:00:17 +0200

    Introduce several optmizations to image handling functions

--- project-based-learning/photomosaic/image_processing/image_processing.cpp
+++ project-based-learning/photomosaic/image_processing/image_processing.cpp
@@ -4,6 +4,7 @@
 #include <Magick++.h>
 #include "utilities.h"
 #include "image_processing.h"
+using namespace Magick;
 
 pixels_array preprocess_image(Magick::Image & image, size_t res_x, size_t res_y) {
     size_t width = image.columns(), height = image.rows();
@@ -12,11 +13,11 @@ pixels_array preprocess_image(Magick::Image & image, size_t res_x, size_t res_y)
     pixels_array average(row_blocks, std::vector<std::vector<float>>(col_blocks, std::vector<float>(3, 0))); 
     image.modifyImage();
     for (size_t i=0; i<height; i++){
+        const Magick::PixelPacket* pixels = image.getConstPixels(0, i, width, 1); 
         for (size_t j=0; j<width; j++){
-            Magick::ColorRGB px = image.pixelColor(j, i);
-            average[i/res_y][j/res_x][0] += px.red();
-            average[i/res_y][j/res_x][1] += px.green();
-            average[i/res_y][j/res_x][2] += px.blue();
+            average[i/res_y][j/res_x][0] += pixels[j].red/(float)QuantumRange;
+            average[i/res_y][j/res_x][1] += pixels[j].green/(float)QuantumRange;
+            average[i/res_y][j/res_x][2] += pixels[j].blue/(float)QuantumRange;
         }
     }
     for (size_t i=0; i<row_blocks; i++){
@@ -49,25 +50,32 @@ void crop_photo(Magick::Image& image, std::string& output_folder, std::string& i
     image.write(output_folder+"/"+image_name);
 }
 
-
 void construct_image_from_files(Magick::Image& image, files_array& files, size_t res_x, size_t res_y){
     size_t rows = files.size(), cols = files[0].size();
+    size_t width = image.columns();
+    image_map sources_cache;
     for (size_t i=0; i < rows; i++){
+        Magick::PixelPacket *pixel_cache = image.getPixels(0, i*res_x, width, res_x);
         for (size_t j=0; j < cols; j++){
-            Magick::Image src_img(files[i][j]);
+            Magick::Image src_img;
+            if (sources_cache.find(files[i][j]) == sources_cache.end()){
+                src_img.read(files[i][j]);
+                src_img.resize(Magick::Geometry(res_y, res_x));
+                sources_cache[files[i][j]] = src_img;
+            }
+            else{
+                src_img = sources_cache[files[i][j]];
+            }
             src_img.resize(Magick::Geometry(res_y, res_x));
-            //image.type(Magick::TrueColorType);
-            //src_img.type(Magick::TrueColorType);
             src_img.modifyImage();
             image.modifyImage();
-            Magick::PixelPacket *pixel_cache = image.getPixels(j*res_y, i*res_x, res_y, res_x);
-            Magick::PixelPacket *src_pixel_cache = src_img.getPixels(0, 0, res_y, res_x);
-            for (size_t k=0; k < res_y; k++){
-                for (size_t l=0; l < res_x; l++){
-                    pixel_cache[l+k*res_x] = src_pixel_cache[l+k*res_x];
+            const Magick::PixelPacket *src_pixel_cache = src_img.getConstPixels(0, 0, res_y, res_x);
+            for (size_t k=0; k < res_x; k++){
+                for (size_t l=0; l < res_y; l++){
+                    pixel_cache[l+k*width+j*res_y] = src_pixel_cache[l+k*res_y];
                 }
             }
-            image.syncPixels(); 
         }
+        image.syncPixels(); 
     }
 }
