0d96ca7dbc227b4555f2fe6f8a882f783741ff9c
cescgina <cescgina@gmail.com>
2019-10-05 19:20:12 +0200

    Add cli for game_of_life

--- project-based-learning/game_of_life/game_of_life.cpp
+++ project-based-learning/game_of_life/game_of_life.cpp
@@ -1,10 +1,35 @@
+#include <string>
+#include <iostream>
+#include <unistd.h>
 #include "board.h"
 
 
+void print_args(){
+    std::cerr << "Usage is: ./game_of_life.o width height" << std::endl;
+    std::cerr << "Input parameters description:" << std::endl; 
+    std::cerr << "\twidth: Board width (in characters)" << std::endl;
+    std::cerr << "\theight: Board height (in characters)" << std::endl;
+}
+
 int main(int argc, char** argv){
-    int w = 10, h = 10;
-    Board game_board = Board(w, h);
+    if (argc < 3) {
+        // TODO: Finish cli description
+        std::cerr << "Unsufficient number of arguments!!" << std::endl;
+        print_args();
+        return -1;
+    }
+    int c;
+    while ((c = getopt (argc, argv, "h")) != -1){
+        switch (c){
+          case 'h':
+            print_args();
+            return -1;
+        }
+    }
+    int width = std::stoi(std::string(argv[optind])), height = std::stoi(std::string(argv[optind+1]));
+    Board game_board = Board(width, height);
     game_board.printBoard();
     game_board.stepBoard();
     game_board.printBoard();
+
 }
