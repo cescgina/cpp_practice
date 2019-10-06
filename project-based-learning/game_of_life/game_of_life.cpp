452ffe58bba666636a2f3170d6e18b6bd6111448
cescgina <cescgina@gmail.com>
2019-10-06 13:27:52 +0200

    Add running of several iterations to main

--- project-based-learning/game_of_life/game_of_life.cpp
+++ project-based-learning/game_of_life/game_of_life.cpp
@@ -9,10 +9,11 @@ void print_args(){
     std::cerr << "Input parameters description:" << std::endl; 
     std::cerr << "\twidth: Board width (in characters)" << std::endl;
     std::cerr << "\theight: Board height (in characters)" << std::endl;
+    std::cerr << "\titerations: Number of iterations to run" << std::endl;
 }
 
 int main(int argc, char** argv){
-    if (argc < 3) {
+    if (argc < 4) {
         // TODO: Finish cli description
         std::cerr << "Unsufficient number of arguments!!" << std::endl;
         print_args();
@@ -27,9 +28,12 @@ int main(int argc, char** argv){
         }
     }
     int width = std::stoi(std::string(argv[optind])), height = std::stoi(std::string(argv[optind+1]));
+    int n_iterations = std::stoi(std::string(argv[optind+2]));
     Board game_board = Board(width, height);
     game_board.printBoard();
-    game_board.stepBoard();
-    game_board.printBoard();
+    for (int i=0; i < n_iterations; i++){
+        game_board.stepBoard();
+        game_board.printBoard();
+    }
 
 }
