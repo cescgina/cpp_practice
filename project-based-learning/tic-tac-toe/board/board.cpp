6a05963ebd91dbb61927d72fabf4c67b66ee91c2
cescgina <cescgina@gmail.com>
2019-10-13 16:42:39 +0200

    Check for victory in reverse diagonal
    Also remove unneeded code

--- project-based-learning/tic-tac-toe/board/board.cpp
+++ project-based-learning/tic-tac-toe/board/board.cpp
@@ -4,63 +4,20 @@
 #include <iostream>
 #include "board.h"
 
-char const* FileTooSmallException::what () const noexcept{
-    return "Provided file is too small!";
-}
-
-char const* FileTooBigException::what () const noexcept{
-    return "Provided file is too big!";
-}
-
-char const* FileNotFoundException::what () const noexcept{
-    return "Provided file could not be opened!";
-}
-
 Board::Board(int width, int height){
     board_width = width;
     board_height = height;
     player_1 = 'X';
     player_2 = 'O';
     board = std::vector<std::vector<int>>(height, std::vector<int>(width, 0));
-    has_printed = false;
 }
 
 Board::Board(board_matrix& new_board){
     board_height = new_board.size();
     board_width = new_board[0].size();
     board = std::move(new_board);
-    has_printed = false;
     player_1 = 'X';
     player_2 = 'O';
-}
-
-Board::Board(const std::string& path_file){
-    has_printed = false;
-    player_1 = 'X';
-    player_2 = 'O';
-    board = std::vector<std::vector<int>>();
-    std::vector<int> board_row;
-    std::ifstream saved_board;
-    saved_board.open(path_file);
-    if (!saved_board.is_open()){
-        throw FileNotFoundException();
-    }
-    char temp;
-    while (saved_board.get(temp)){
-        if (temp <= '9' and temp >= '0') {
-            board_row.push_back(temp-'0');
-        }
-        else{
-            // whitespace or newline, if we have a line stored, update the
-            // board matrix, else do nothing
-            if(board_row.size() > 0){
-                board.push_back(board_row);
-                board_row = {};
-            }
-        }
-    }
-    board_height = board.size();
-    board_width = board[0].size();
 }
 
 void Board::printBoard(){
@@ -96,35 +53,6 @@ void Board::printBoard(){
     }
     std::cout << std::endl;
 }
-/*
-void Board::printBoard(){
-    if (has_printed){
-        wmove(win, 1, 1);
-    }
-    else{
-        win = newwin(board_height+2, board_width+2, 0, 0);
-        wmove(win, 0, 0);
-        wborder(win, 0, 0, 0, 0, 0, 0, 0, 0);
-        wmove(win, 1, 1);
-    }
-    for (int i=0; i<board_height; i++){
-        if (!has_printed){
-            wmove(win, i+1, 1);
-        }
-        else{
-            wmove(win, i+1, 1);
-        }
-        for (int j=0; j<board_width; j++){
-           if (board[i][j] == 0) waddch(win, ' ');
-           else waddch(win, '#');
-        }
-    }
-    if (!has_printed){
-        has_printed = true;
-    }
-    wrefresh(win);
-} 
-*/
 
 bool Board::gameTurn(){
     int x, y;
@@ -138,6 +66,10 @@ bool Board::gameTurn(){
             std::cout << "Congratulations Player 1! You win!!" << std::endl;
             return true;
         }
+    }
+    if (!hasMoves()){
+        printTie();
+        return true;
     }
     printBoard();
     validMove = false;
@@ -193,7 +125,7 @@ bool Board::checkVictory(int x, int y, int player){
             return true;
         }
     } 
-    //check diagonal match
+    //check main diagonal match
     int i=lower_row, j=lower_col; 
     while (i<higher_row and j<higher_col){
         if (board[i][j] == player and board[i+1][j+1] == player and board[i+2][j+2] == player){
@@ -202,52 +134,20 @@ bool Board::checkVictory(int x, int y, int player){
         i++;
         j++;
     }
+    //check reverse diagonal match
+    i=higher_row+1, j=lower_col;
+    while (i>=lower_row and j<higher_col){
+        if (board[i][j] == player and board[i-1][j+1] == player and board[i-2][j+2] == player){
+            return true;
+        }
+        i--;
+        j++;
+    }
     return false;
 }
 
 void Board::setBoard(board_matrix& new_board){
    board = std::move(new_board);
-}
-
-void Board::setBoard(const std::string& path_file){
-    std::ifstream saved_board;
-    char temp;
-    saved_board.open(path_file);
-    if (!saved_board.is_open()){
-        throw FileNotFoundException();
-    }
-    for (int i=0; i<board_height; i++){
-        for (int j=0; j<board_width; j++){
-            if (saved_board.eof()){
-                throw FileTooSmallException();
-            }
-            saved_board >> temp;
-            board[i][j] = temp-'0';
-        }
-    }
-    int a, c=0;
-    while (saved_board){
-        saved_board >> a;
-        c++;
-    }
-    // if there were more than one reads possible, then the wrong board dimensions
-    // were specified (the last read was the newline)
-    if(c > 1){
-        throw FileTooBigException();
-    }
-    saved_board.close();
-}
-
-void Board::saveBoard(const std::string& path_file){
-    std::ofstream saved_board;
-    saved_board.open(path_file);
-    for (int i=0; i<board_height; i++){
-        for (int j=0; j<board_width; j++){
-            saved_board << board[i][j];
-        }
-        saved_board << std::endl;
-    }
-    saved_board.close();
 }
 
 board_matrix Board::getBoard(){
@@ -260,4 +160,8 @@ int Board::getWidth(){
 
 int Board::getHeight(){
    return board_height;
+}
+
+void Board::printTie(){
+    std::cout << "There are no more possible moves, game over!!!" << std::endl;
 }
--- project-based-learning/tic-tac-toe/board/board.h
+++ project-based-learning/tic-tac-toe/board/board.h
@@ -1,47 +1,27 @@
 #include <string>
 #include <vector>
 #include <exception>
-#include <ncurses.h>
 
 typedef std::vector<std::vector<int>> board_matrix;
-
-class FileNotFoundException : public std::exception {
- public:
-    virtual char const* what () const noexcept;
-};
-
-class FileTooBigException : public std::exception {
- public:
-    virtual char const* what () const noexcept;
-};
-
-class FileTooSmallException : public std::exception {
- public:
-    virtual char const* what () const noexcept;
-};
 
 class Board{
     public:
         Board(int width, int height);
         Board(board_matrix& new_board);
-        Board(const std::string& path_file);
         void printBoard();
         bool gameTurn();
         void setBoard(board_matrix& b);
-        void setBoard(const std::string& path_file);
-        void saveBoard(const std::string& path_file);
         board_matrix getBoard();
         int getWidth();
         int getHeight();
         bool hasMoves();
+        void printTie();
+        bool checkVictory(int x, int y, int player);
+        bool makeMove(int x, int y, int player);
     private:
         board_matrix board;
         char player_1;
         char player_2;
-        bool has_printed;
         int board_width;
         int board_height;
-        bool checkVictory(int x, int y, int player);
-        bool makeMove(int x, int y, int player);
-        //WINDOW * win;
 };
